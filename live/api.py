0efc4bde2089e8a81203c258af17bf68d9c98326
Esubalew Chekol <esubalewchekol6@gmail.com>
2025-03-28 11:56:28 +0300

    Update api.py

--- live/api.py
+++ live/api.py
@@ -1,12 +1,9 @@
-import requests
+import aiohttp
+import asyncio
 
 BASE_URL = 'https://estate-r22a.onrender.com/live'
 
-
-# Add a new request
-
-
-def create_request(user_id, username, name, phone, address, additional_text):
+async def create_request(user_id, username, name, phone, address, additional_text):
     data = {
         "user_id": user_id,
         "username": username,
@@ -15,31 +12,32 @@ def create_request(user_id, username, name, phone, address, additional_text):
         "address": address,
         "additional_text": additional_text
     }
-    response = requests.post(f'{BASE_URL}/requests/', json=data)
-    return response.json() if response.status_code == 201 else None
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'{BASE_URL}/requests/', json=data) as response:
+            return await response.json() if response.status == 201 else None
 
-# Add a new message
-def create_message(request_id, sender_id, user_id, content):
+async def create_message(request_id, sender_id, user_id, content):
     data = {
         "request": request_id,
         "sender_id": sender_id,
         "user_id": user_id,
         "content": content
     }
-    response = requests.post(f'{BASE_URL}/messages/', json=data)
-    return response.json() if response.status_code == 201 else None
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'{BASE_URL}/messages/', json=data) as response:
+            return await response.json() if response.status == 201 else None
 
-# Get all requests
-def get_all_requests():
-    response = requests.get(f'{BASE_URL}/requests/')
-    return response.json() if response.status_code == 200 else []
+async def get_all_requests():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{BASE_URL}/requests/') as response:
+            return await response.json() if response.status == 200 else []
 
-# get request by ID
-def get_request_details(request_id):
-    response = requests.get(f'{BASE_URL}/requests/{request_id}/')
-    return response.json() if response.status_code == 200 else None
+async def get_request_details(request_id):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{BASE_URL}/requests/{request_id}/') as response:
+            return await response.json() if response.status == 200 else None
 
-# get all messages
-def get_all_messages():
-    response = requests.get(f'{BASE_URL}/messages/')
-    return response.json() if response.status_code == 200 else []
+async def get_all_messages():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{BASE_URL}/messages/') as response:
+            return await response.json() if response.status == 200 else []
